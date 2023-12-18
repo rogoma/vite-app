0c8db19023863097777445afc42fe12e589d2fb8
rogoma <rogoma007@gmail.com>
2023-12-17 22:47:15 -0300

    Actualiza con React

--- resources/js/app.js
+++
@@ -1,7 +0,0 @@
-import './bootstrap';
-
-
-import '../sass/app.scss'
-
-
-import * as bootstrap from 'bootstrap'
--- vite.config.js
+++ vite.config.js
@@ -1,20 +1,13 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
-import path from 'path';
-
+import react from '@vitejs/plugin-react';
 
 export default defineConfig({
     plugins: [
         laravel({
-            input: [              
-                'resources/js/app.js',
-            ],
+            input: 'resources/js/app.jsx',
             refresh: true,
         }),
+        react(),
     ],
-    resolve: {
-        alias:{
-            '~bootstrap': path.resolve(__dirname, 'node_modules/bootstrap'),
-        }
-    }
 });
